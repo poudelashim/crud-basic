46f33bcf502d5ee5c43afaa2c59e81655b674070
Ashim Poudel <poudelashim26@gmail.com>
2022-09-15 12:29:59 +1000

    create post

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const form = document.querySelector(".form");
 const textArea = document.querySelector(".textarea");
 const buttonPost = document.querySelector(".btn-submit");
 const msg = document.querySelector(".msg");
+const posts = document.querySelector(".right");
 
 buttonPost.addEventListener("click", (e) => {
   e.preventDefault();
@@ -16,5 +17,26 @@ const formValidation = () => {
   } else {
     console.log("pass");
     msg.innerHTML = "";
+    addData();
   }
+};
+
+const data = {};
+const addData = () => {
+  data["text"] = textArea.value;
+  console.log(data);
+  createPost();
+};
+
+const createPost = () => {
+  posts.innerHTML += `
+  <div class="container-right">
+  <h5>${data.text}</h5>
+  <div class="options">
+      <i class="fa-regular fa-pen-to-square"></i>
+      <i class="fa-solid fa-trash"></i>
+  </div>
+</div>
+  `;
+  textArea.value = "";
 };
